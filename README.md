4e36d6853fe4fae8124530b883a9125de11051c2
ahmadhulaifi <ahmadhulaifi1@gmail.com>
2020-08-23 23:03:08 +0700

    menambahkan file database

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Sistem-Login-CI-4
+
 Ini adalah Repository tentang sistem login menggunakan framework Codeigniter 4 yang mana didalamnya terdapat sistem registrasi, login, menu management, role Akses
+
+# Konfigurasi Awal
+
+1. silahkan pindahkan folder kalian di localhost atau hosting
+2. Silahkan import file sql yang ada didalam folder database ke phpmyadmin atau program dbms lainnya
+3. Untuk kofigurasinya dalam codeigniter 4 silahkan buka file .env yang ada didalam folder core_loginci4
+4. Ubah app.baseURL sesuai dengan tempat public kalian
+5. Setting juga database dengan username dan password yang kalian gunakan di localhost atau hosting kalian
+6. Jika ingin mendevelop program ini bisa ubah CI_ENVIRONMENT di dalam folder env untuk melihat jika ada kesalahan error
+
+# Hal yang diperhatikan
+
+1. di program ini sudah ditambahkan file filter didalam folder app/Filters/LoginFilter untuk security tidak bisa masuk jika belum login dan juga membatasi akses untuk role akses membernya. Jika tidak ingin digunakan bisa hapus filenya dan hapus juga aturan di route dengan filter ceklogindulu
+2. Dalam database user is_active 0 artinya belum aktif dan 1 sudah aktif berfungsi jika teman-teman ingin menambahkan registrasi dengan verifikasi email
---
+++ database/login.sql
@@ -0,0 +1,212 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 23, 2020 at 05:52 PM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `is_active` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `email`, `password`, `role_id`, `is_active`, `image`, `created_at`, `updated_at`) VALUES
+(29, 'ahmad', 'ahmad@gmail.com', '$2y$10$V/AtYTg0CpY3xAgEEEz2x.xumsw0SbK3PExr38I.0IczIPv3nwI6W', 1, 1, 'default.png', '2020-08-19 12:27:36', '2020-08-19 12:27:36'),
+(30, 'hulaifi', 'hulaifi@gmail.com', '$2y$10$5bA.y9DXdlhX9iN0r.Xsw.a2aBS6De0zlLQZ65fh4jAJYGTInfl2u', 2, 1, 'default.png', '2020-08-19 23:16:35', '2020-08-19 23:16:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_access_menu`
+--
+
+CREATE TABLE `user_access_menu` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(3, 2, 2),
+(4, 1, 3),
+(8, 1, 1),
+(9, 1, 2),
+(12, 1, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(255) NOT NULL,
+  `icon` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`, `icon`) VALUES
+(1, 'Admin', 'fas fa-fw fa-user'),
+(2, 'User', 'fas fa-fw fa-users'),
+(3, 'Menu', 'fas fa-fw fa-folder');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `role`) VALUES
+(1, 'Admin'),
+(2, 'Member');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_sub_menu`
+--
+
+CREATE TABLE `user_sub_menu` (
+  `id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `sub_menu` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `icon` varchar(255) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_sub_menu`
+--
+
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `sub_menu`, `url`, `icon`, `is_active`) VALUES
+(1, 1, 'sub menu admin', 'admin', 'fa fa-fw fa-tachometer-alt', 1),
+(2, 2, 'sub menu user', 'member', 'fas fa-fw fa-user', 1),
+(3, 3, 'Menu Management', 'menu', 'far fa-fw fa-folder-open', 1),
+(4, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
+(7, 3, 'Role Management', 'menu/role', 'fas fa-fw fa-users-cog', 1),
+(8, 4, 'tes submenu', 'adad', 'adawda', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
